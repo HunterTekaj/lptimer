2b00079b106c23ce1f945e3dc9744814d84f797c
HunterTekaj <oficialmustafa0@gmail.com>
2023-10-26 14:43:01 -0300

    version 1

---
+++ lptimer.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+    echo "Usage: lptimer [message] [-H] (hours) [-M] (minutes) [-S] (seconds)"
+    exit 1
+fi
+
+HOURS="0"
+MINUTES="0"
+SECS="0"
+while getopts ":H:M:S:" options; do
+    case "${options}" in
+        H)
+            HOURS=${OPTARG}
+            ;;
+        M)
+            MINUTES=${OPTARG}
+            ;;
+        S)
+            SECS=${OPTARG}
+            ;;
+        :)
+            echo -e "lptimer: \e[1;31mError: ${OPTARG} requires an argument"
+            exit 2
+            ;;
+        *)
+            exit 4
+            ;;
+    esac
+done
+
+ITERATION=1
+while true; do
+    echo "lptimer: iteration = $ITERATION"
+    if [ "$HOURS" != "0" ]; then
+        # echo "lptimer: hours: $(($HOURS*3600))"
+        sleep $(($HOURS*3600))
+    fi
+    if [ "$MINUTES" != "0" ]; then
+        # echo "lptimer: minutes: $(($MINUTES*60))"
+        sleep $(($MINUTES*60))
+    fi
+    if [ "$SECS" != "0" ]; then
+        # echo "lptimer: seconds: $SECS"
+        sleep $SECS
+    fi
+    ITERATION=$(($ITERATION+1))
+done
+
